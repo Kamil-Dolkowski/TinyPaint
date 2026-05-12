016c48f7c232cd2d70938df2743038a3b6aa5e0a
Kamil-Dolkowski <kamil.dolkowski@op.pl>
2026-05-12 10:05:15 +0200

    feat(Interaction Controller): add touch logic - draw delay (gesture or draw delay)

--- js/managers/InteractionController.js
+++ js/managers/InteractionController.js
@@ -1,7 +1,12 @@
 export default class InteractionController {
     constructor(toolManager) {
         this.toolManager = toolManager;
+
         this.isGesture = false;
+
+        this.pointerdownData = null;
+        this.lastPointerData = null;
+        this.maybeDraw = false;
     }
 
     onInput(pointerData, gestureData) {
@@ -14,15 +19,44 @@ export default class InteractionController {
         } else {
             this.isGesture = false;
 
-            // delay for pointerdown
-            if (pointerData.eventType == "pointerdown") {
-                setTimeout(() => {
-                    if (!this.isGesture) {
-                        this.toolManager.onInputPointer(pointerData);
-                    }
-                }, 20);
+            // touch logic
+            if (pointerData.pointerType == "touch") {
 
-                return;
+                // delay for pointerdown
+                if (pointerData.eventType == "pointerdown") {
+                    this.maybeDraw = true;
+                    this.pointerdownData = structuredClone(pointerData);
+
+                    setTimeout(() => {
+                        if (!this.isGesture) {
+                            if (this.lastPointerData) {
+                                // update this.lastPointerData -> difference between this.lastPointerData and this.pointerdownData
+                                this.lastPointerData.last = this.pointerdownData.current;
+                                this.lastPointerData.delta = {
+                                    x: this.lastPointerData.current.x - this.lastPointerData.last.x,
+                                    y: this.lastPointerData.current.y - this.lastPointerData.last.y
+                                };
+
+                                this.toolManager.onInputPointer(this.pointerdownData);
+                                this.toolManager.onInputPointer(this.lastPointerData);
+                            } else {
+                                this.toolManager.onInputPointer(this.pointerdownData);
+                            }
+
+                            this.pointerdownData = null;
+                            this.lastPointerData = null;
+                        } 
+
+                        this.maybeDraw = false;
+                    }, 20);
+
+                    return;
+                }
+
+                if (this.maybeDraw) {
+                    this.lastPointerData = structuredClone(pointerData);
+                    return;
+                }
             }
 
             this.toolManager.onInputPointer(pointerData);
