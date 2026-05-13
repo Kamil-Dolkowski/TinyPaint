ed0b6d9a208f03ae387ab4dbd489ce2ba9c494ea
Kamil-Dolkowski <kamil.dolkowski@op.pl>
2026-05-13 12:31:59 +0200

    feat(keyboard): add holdActions

--- js/managers/ShortcutManager.js
+++ js/managers/ShortcutManager.js
@@ -1,7 +1,9 @@
 export default class ShortcutManager {
-    constructor(history) {
+    constructor(history, toolManager) {
         this.history = history;
+        this.toolManager = toolManager;
 
+        // shortcuts - single action
         this.shortcuts = {
             ctrlZ: {
                 code: "KeyZ",
@@ -14,9 +16,30 @@ export default class ShortcutManager {
                 action: () => this.history.redo()
             },
         };
+
+        // holdActions - constant action
+        this.holdActions = {
+            ctrl: {
+                code: "ControlLeft",
+                keydownAction: () => console.log("moveZoom"),
+                keyupAction: () => console.log("default")
+            }
+        }
     }
 
     onKeyInput(keyData) {
+        // holdActions
+        for (const holdAction of Object.values(this.holdActions)) {
+            if (keyData.code === holdAction.code) {
+                if (keyData.eventType == "keydown") {
+                    holdAction.keydownAction();
+                } else {
+                    holdAction.keyupAction();
+                }
+            }
+        }
+        
+        // shortcuts
         if (keyData.eventType == "keyup") return;
 
         for (const shortcut of Object.values(this.shortcuts)) {
